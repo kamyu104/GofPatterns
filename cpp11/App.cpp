2f8953ac49cf372c49f508944207205efa028371
XzoRit <xzorit@outlook.com>
2013-03-12 18:08:26 +0100

    minor changes

--- cpp11/App.cpp
+++ cpp11/App.cpp
@@ -1,123 +1,123 @@
 // /Users/Tobias/Downloads/checker-270/bin/clang++ -std=c++0x -stdlib=libc++ -I./ -I../ -I/Developer/Library/boost_1_51_0 -o app_cpp11 app.cpp
-// #include <CaffeineBeverage.h>
-// #include <Recipes.h>
-// #include <CoffeeMachine.h>
-// #include <View.h>
-// #include <MilkFoam.h>
-// #include <accu.h>
-// #include <Milk.h>
-// #include <Sugar.h>
-// #include <BeverageFactory.h>
-// #include <Condiment.h>
-// #include <CondimentFactory.h>
+#include <CaffeineBeverage.h>
+#include <Recipes.h>
+#include <CoffeeMachine.h>
+#include <View.h>
+#include <MilkFoam.h>
+#include <accu.h>
+#include <Milk.h>
+#include <Sugar.h>
+#include <BeverageFactory.h>
+#include <Condiment.h>
+#include <CondimentFactory.h>
 
-// #include <functional>
-// #include <vector>
-// #include <memory>
+#include <functional>
+#include <vector>
+#include <memory>
 
-// Boost.MSM tutorial
-// back-end
-#include <boost/msm/back/state_machine.hpp>
-//front-end
-#include <boost/msm/front/state_machine_def.hpp>
-// functors
-#include <boost/msm/front/functor_row.hpp>
-#include <boost/msm/front/euml/common.hpp>
-// for And_ operator
-#include <boost/msm/front/euml/operator.hpp>
+// // Boost.MSM tutorial
+// // back-end
+// #include <boost/msm/back/state_machine.hpp>
+// //front-end
+// #include <boost/msm/front/state_machine_def.hpp>
+// // functors
+// #include <boost/msm/front/functor_row.hpp>
+// #include <boost/msm/front/euml/common.hpp>
+// // for And_ operator
+// #include <boost/msm/front/euml/operator.hpp>
 
-using namespace std;
-namespace msm = boost::msm;
-namespace mpl = boost::mpl;
-using namespace msm::front;
-// for And_ operator
-using namespace msm::front::euml;
+// using namespace std;
+// namespace msm = boost::msm;
+// namespace mpl = boost::mpl;
+// using namespace msm::front;
+// // for And_ operator
+// using namespace msm::front::euml;
 
-struct evt_beverage
-{
-  //  std::function<void()> prepare;
-};
+// struct evt_beverage
+// {
+//   //  std::function<void()> prepare;
+// };
 
-struct quit
-{};
+// struct quit
+// {};
 
-struct evt_condiment
-{
-//  Condiment _;
-};
+// struct evt_condiment
+// {
+// //  Condiment _;
+// };
 
-struct sm_coffeeMachine_ : public msm::front::state_machine_def<sm_coffeeMachine_>
-{
-  template <class Event,class FSM>
-  void on_entry(Event const& ,FSM&) 
-  {
-    std::cout << "entering: sm_coffeeMachine_" << std::endl;
-  }
+// struct sm_coffeeMachine_ : public msm::front::state_machine_def<sm_coffeeMachine_>
+// {
+//   template <class Event,class FSM>
+//   void on_entry(Event const& ,FSM&) 
+//   {
+//     std::cout << "entering: sm_coffeeMachine_" << std::endl;
+//   }
 
-  template <class Event,class FSM>
-  void on_exit(Event const&,FSM& ) 
-  {
-    std::cout << "leaving: sm_coffeeMachine_" << std::endl;
-  }
+//   template <class Event,class FSM>
+//   void on_exit(Event const&,FSM& ) 
+//   {
+//     std::cout << "leaving: sm_coffeeMachine_" << std::endl;
+//   }
 
-  struct state_beverage : public msm::front::state<>
-  {
-    template <class Event,class FSM>
-    void on_entry(Event const&,FSM& ) {std::cout << "entering: state_beverage" << std::endl;}
-    template <class Event,class FSM>
-    void on_exit(Event const&,FSM& ) {std::cout << "leaving: state_beverage" << std::endl;}
-  };
+//   struct state_beverage : public msm::front::state<>
+//   {
+//     template <class Event,class FSM>
+//     void on_entry(Event const&,FSM& ) {std::cout << "entering: state_beverage" << std::endl;}
+//     template <class Event,class FSM>
+//     void on_exit(Event const&,FSM& ) {std::cout << "leaving: state_beverage" << std::endl;}
+//   };
 
-  struct state_condiment : public msm::front::state<>
-  {
-    template <class Event,class FSM>
-    void on_entry(Event const&,FSM& ) {std::cout << "entering: state_condiment" << std::endl;}
-    template <class Event,class FSM>
-    void on_exit(Event const&,FSM& ) {std::cout << "leaving: state_condiment" << std::endl;}
-  };
+//   struct state_condiment : public msm::front::state<>
+//   {
+//     template <class Event,class FSM>
+//     void on_entry(Event const&,FSM& ) {std::cout << "entering: state_condiment" << std::endl;}
+//     template <class Event,class FSM>
+//     void on_exit(Event const&,FSM& ) {std::cout << "leaving: state_condiment" << std::endl;}
+//   };
 
-  typedef state_beverage initial_state;
+//   typedef state_beverage initial_state;
 
-  struct action_add_beverage
-  {
-    template <class EVT,class FSM,class SourceState,class TargetState>
-    void operator()(EVT const& ,FSM& ,SourceState& ,TargetState& )
-    {
-      cout << "action_add_beverage" << endl;
-    }
-  };
+//   struct action_add_beverage
+//   {
+//     template <class EVT,class FSM,class SourceState,class TargetState>
+//     void operator()(EVT const& ,FSM& ,SourceState& ,TargetState& )
+//     {
+//       cout << "action_add_beverage" << endl;
+//     }
+//   };
 
-  struct action_add_condiment
-  {
-    template <class EVT,class FSM,class SourceState,class TargetState>
-    void operator()(EVT const& ,FSM& ,SourceState& ,TargetState& )
-    {
-      cout << "action_add_condiment" << endl;
-    }
-  };
+//   struct action_add_condiment
+//   {
+//     template <class EVT,class FSM,class SourceState,class TargetState>
+//     void operator()(EVT const& ,FSM& ,SourceState& ,TargetState& )
+//     {
+//       cout << "action_add_condiment" << endl;
+//     }
+//   };
 
-  struct transition_table : mpl::vector
-    <
-      //         Start            Event             Next             Action               Guard
-      //       +----------------+-----------------+----------------+---------------------+----------------------+
-      Row      < state_beverage , evt_beverage    , state_condiment, action_add_beverage , none                 >,
-      Row      < state_condiment, evt_condiment   , none           , action_add_condiment, none                 >
-    >
-  {};
-};
+//   struct transition_table : mpl::vector
+//     <
+//       //         Start            Event             Next             Action               Guard
+//       //       +----------------+-----------------+----------------+---------------------+----------------------+
+//       Row      < state_beverage , evt_beverage    , state_condiment, action_add_beverage , none                 >,
+//       Row      < state_condiment, evt_condiment   , none           , action_add_condiment, none                 >
+//     >
+//   {};
+// };
 
-typedef msm::back::state_machine<sm_coffeeMachine_> coffeeMachine;
+// typedef msm::back::state_machine<sm_coffeeMachine_> coffeeMachine;
 
-int main()
-{
-  coffeeMachine c;
-  c.start();
+// int main()
+// {
+//   coffeeMachine c;
+//   c.start();
 
-  c.process_event(evt_beverage());
-c.process_event(evt_condiment());
+//   c.process_event(evt_beverage());
+//   c.process_event(evt_condiment());
 
-  c.stop();
-}
+//   c.stop();
+// }
 // Boost.Signals2 tutorial
 // #include <boost/signals2.hpp>
 // #include <boost/shared_ptr.hpp>
@@ -195,11 +195,11 @@ c.process_event(evt_condiment());
 //   2
 //      */
 //   }
-/*
+
 int main(int argc, char* argv[])
 {
   using namespace std;
-  using namespace cpp11;*/
+  using namespace cpp11;
   /* { // bind
     using Beverages = std::vector<std::unique_ptr<CaffeineBeverage>>;
     Beverages beverages;
@@ -239,7 +239,7 @@ int main(int argc, char* argv[])
 	  }
       }
   }*/
-  /*  { // lambda
+  { // lambda
     using Beverages = std::vector<std::unique_ptr<CaffeineBeverage>>;
     Beverages beverages;
     CoffeeMachine coffeeMachine;
@@ -277,7 +277,8 @@ int main(int argc, char* argv[])
 	    beverage->price();
 	  }
       }
-      }*/
+   }
+
   /*
   using Beverages = std::vector<std::unique_ptr<CaffeineBeverage>>;
   Beverages beverages;
@@ -320,5 +321,5 @@ int main(int argc, char* argv[])
   std::cout << elapsed.count() << '\n';
   std::string dummy;
   view.askForCondiments(dummy);*/
-//}
+}
 
